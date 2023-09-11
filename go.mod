68c2ca611bd05cbe42ac6c0f70b5b0978a72322b
Umar Ma'ruf Mutaqin <umarmarufmutaqin@gmail.com>
2023-09-11 15:24:24 +0700

    ok

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.9.0
 	github.com/go-playground/validator/v10 v10.12.0
-	github.com/gorilla/mux v1.8.0
 	github.com/spf13/viper v1.15.0
 	github.com/stretchr/testify v1.8.2
 	gorm.io/driver/mysql v1.4.7
--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"net/http"
 
 	"auto-emails/app"
+	"auto-emails/auth"
 	c "auto-emails/configuration"
+	"auto-emails/controller"
 	"auto-emails/helper"
+	"auto-emails/repository"
+	"auto-emails/service"
 
+	"github.com/gin-gonic/gin" // Import Gin
 	"github.com/go-playground/validator/v10"
-	// Import Gorilla Mux router
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -25,18 +30,14 @@ func main() {
 	validate := validator.New()
 	helper.RegisterValidation(validate)
 
-	// Create a handler function for your routes
-	router := app.NewRouter(dbMS, dbMY, validate)
-
-	// Define your routes using http.HandleFunc
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		router.ServeHTTP(w, r)
-	})
+	// Create a Gin router
+	router := NewRouter(dbMS, dbMY, validate)
 
 	// Create a server and listen on the desired port
 	port := configuration.Port
 	server := &http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
+		Handler: router,
 	}
 
 	log.Printf("Server is running on port %s", port)
@@ -45,4 +46,27 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func NewRouter(dbMS *gorm.DB, dbMY *gorm.DB, validate *validator.Validate) *gin.Engine {
+	router := gin.Default() // Use Gin in "release" mode
+
+	// Your Gin route setup here
+	EmailRoute(router, dbMS, dbMY, validate)
+
+	return router
+}
+
+func EmailRoute(router *gin.Engine, dbMS *gorm.DB, dbMY *gorm.DB, validate *validator.Validate) {
+	emailService := service.NewEmailService(
+		repository.NewEmailRepository(),
+		dbMS,
+		dbMY,
+		validate,
+	)
+	emailController := controller.NewEmailController(emailService)
+
+	// Define your Gin routes here
+	router.GET("/", auth.Auth(emailController.EmailProsess, []string{}))
+	// Add more routes as needed
 }
